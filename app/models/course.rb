770bddd9f56be0ebc42e1b20a3ffe78a439b3184
Jorge Ramos <jorgeramos2068@gmail.com>
2020-04-07 09:21:50 -0600

    Add model and migration for Courses

---
+++ app/models/course.rb
@@ -0,0 +1,5 @@
+class Course < ApplicationRecord
+  validate :short_name, presence: true, length: { minumum: 3, maximum: 15 }
+  validate :name, presence: true, length: { minumum: 5, maximum: 50 }
+  validates :description, presence: true, length: { minumum: 10, maximum: 300 }
+end
