492d6b0b83a574c36119892e4e1b37e727d0dac2
Jorge Ramos <jorgeramos2068@gmail.com>
2020-04-14 09:35:25 -0600

    Add sessions functionality and login/logout operations

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    student = Student.find_by(email: params[:session][:email].downcase)
+    if student && student.authenticate(params[:session][:password])
+      session[:student_id] = student.id
+      flash[:notice] = "You have successfully logged in."
+      redirect_to student
+    else
+      flash.now[:notice] = "Something was wrong with your login information."
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:student_id] = nil
+    flash[:notice] = "You have successfully logged out"
+    redirect_to root_path
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :email, :password, :password_confirmation)
+  end
+end
