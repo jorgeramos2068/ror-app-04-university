d99f23e44da3724e5a4154f435ba6dba213fc883
Jorge Ramos <jorgeramos2068@gmail.com>
2020-04-15 10:28:18 -0600

    Add many to many association between students and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ApplicationRecord
   validates :short_name, presence: true, length: { minumum: 3, maximum: 15 }
   validates :name, presence: true, length: { minumum: 5, maximum: 50 }
   validates :description, presence: true, length: { minumum: 10, maximum: 300 }
+  has_many :student_courses
+  has_many :students, through: :student_courses
 end
