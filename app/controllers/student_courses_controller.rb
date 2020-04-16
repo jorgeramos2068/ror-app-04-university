28d22712c4b4a858e74366847edf1c5dd20e5a20
Jorge Ramos <jorgeramos2068@gmail.com>
2020-04-16 11:47:31 -0600

    Add enrollment functionality

---
+++ app/controllers/student_courses_controller.rb
@@ -0,0 +1,13 @@
+class StudentCoursesController < ApplicationController
+  def create
+    course = Course.find(params[:course_id])
+    unless current_user.courses.include?(course)
+      StudentCourse.create(course: course, student: current_user)
+      flash[:notice] = "You have successfully enrolled in #{course.name}"
+      redirect_to current_user
+    else
+      flash[:notice] = "Something was wrong with your enrollment"
+      redirect_to root_path
+    end
+  end
+end
